4e73aad796b5aa5548efe540e0769b017617c690
impronen <impronen@gmail.com>
2023-06-26 13:43:30 +0300

    tutorial complete

--- src/App.js
+++ src/App.js
@@ -1,15 +1,56 @@
 import React, { Component } from "react";
 import Overview from "./components/Overview";
+import uniqid from "uniqid";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
+
+    this.state = {
+      task: {
+        text: "",
+        id: uniqid(),
+      },
+      tasks: [],
+    };
   }
 
+  handleChange = (e) => {
+    this.setState({
+      task: {
+        text: e.target.value,
+        id: this.state.task.id,
+      },
+    });
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: {
+        text: "",
+        id: uniqid(),
+      },
+    });
+  };
+
   render() {
+    const { task, tasks } = this.state;
+
     return (
       <div>
-        <Overview title="React" />
+        <form onSubmit={this.onSubmitTask}>
+          <label htmlFor="taskInput">Enter task</label>
+          <input
+            onChange={this.handleChange}
+            value={task.text}
+            type="text"
+            id="taskInput"
+          />
+          <button type="submit">Add Task</button>
+        </form>
+        <Overview tasks={tasks} />
       </div>
     );
   }
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,13 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Overview extends Component {
-  constructor(props) {
-    super(props);
-  }
+const Overview = (props) => {
+  const { tasks } = props;
 
-  render() {
-    return <div></div>;
-  }
-}
+  return (
+    <ul>
+      {tasks.map((task) => {
+        return <li key={task.id}>{task.text}</li>;
+      })}
+    </ul>
+  );
+};
 
 export default Overview;
